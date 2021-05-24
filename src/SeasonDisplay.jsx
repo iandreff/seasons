a9d20a2d3b6b2c79634300d4ff5303ecfa2b744a
Ygor Andreff <iandreff@gmail.com>
2021-05-23 19:25:34 -0600

    My first commit

---
+++ src/SeasonDisplay.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SeasonDisplay = (props) => {
+    return (
+        <div className="">Latitud: {props.Latitud}</div>
+      );
+}
+ 
+export default SeasonDisplay;
